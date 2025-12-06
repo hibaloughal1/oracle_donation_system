8f23e0ec1d84ac7174be6e30f0b9032fcf2f9a40
yassinembasskar <moutaoikkilbasskaryassine@gmail.com>
2025-12-07 00:06:13 +0100

    database tables

---
+++ database/ddl/seed_demo_data.sql
@@ -0,0 +1,95 @@
+
+0 rows deleted.
+
+
+0 rows deleted.
+
+
+0 rows deleted.
+
+
+0 rows deleted.
+
+
+0 rows deleted.
+
+
+0 rows deleted.
+
+
+0 rows deleted.
+
+
+0 rows deleted.
+
+
+Commit complete.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+Commit complete.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+Commit complete.
+
+    FROM Utilisateur
+    *
+ERROR at line 17:
+ORA-06550: line 17, column 5:
+PL/SQL: ORA-00926: missing VALUES keyword
+ORA-06550: line 16, column 5:
+PL/SQL: SQL Statement ignored
+ORA-06550: line 21, column 5:
+PL/SQL: ORA-00926: missing VALUES keyword
+ORA-06550: line 20, column 5:
+PL/SQL: SQL Statement ignored
+
+https://docs.oracle.com/error-help/db/ora-06550/
+
+
+More Details :
+https://docs.oracle.com/error-help/db/ora-06550/
+
+PL/SQL procedure successfully completed.
+
+
+PL/SQL procedure successfully completed.
+
+
+PL/SQL procedure successfully completed.
+
+
+PL/SQL procedure successfully completed.
+
+
+PL/SQL procedure successfully completed.
+
